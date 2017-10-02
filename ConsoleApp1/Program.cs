8ff594a37c8bc2da4d644089969132b718880eda
RodReyesJr <reyesrodjr@gmail.com>
2017-10-01 21:43:02 -0700

    commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Hello from Console!");
+            System.Console.ReadKey();
         }
     }
 }
